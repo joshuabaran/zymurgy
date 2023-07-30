205b41911205684a374384f69ac759c30bad8bbb
Joshua Baran <joshua.baran@gmail.com>
2023-07-30 01:03:12 -0600

    do the things

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
-export function brixToSG(brix: number): number {
-  return (brix * 4 / 1000) + 1;
-}
-
-export function sgToBrix(sg: number): number {
-  return sg / 4;
-}
+export * from './conversions'
